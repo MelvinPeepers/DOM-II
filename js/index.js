c8dcc1641fa8eebf2325bffa104e97f5dd22f39e
Melvin Peepers <peepersm@gmail.com>
2019-05-03 19:17:34 -0400

    Created two more events one for the buttons and one for the h2 headers

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-// #1 mouseover event listener
+// #1 mouseover event listener on nav list
 const navList = document.querySelectorAll('nav a');
 // console.log(navList);
 navList.forEach((link, i) => {
@@ -14,3 +14,26 @@ navList.forEach((link, i) => {
 });
 
 
+// #2 click event listener on buttons
+const destinationBtn = document.querySelectorAll('.btn');
+//console.log(destinationBtn);
+destinationBtn.forEach((btn, i) => {
+    btn.addEventListener("click", function() {
+        alert('Thank you for clicking!');
+    },
+        {once: true}
+    );
+});
+
+// #3 mouseover event listener: text changes color on all h2 elements
+const headerTwo = document.querySelectorAll('h2');
+// console.log(navList);
+headerTwo.forEach((link, i) => {
+    link.addEventListener("mouseover", function() {
+        link.style.color = '#FFBC4E';
+
+        setTimeout(function() {
+            link.style.color = "#000";
+        }, 600);
+    }, false);
+});
