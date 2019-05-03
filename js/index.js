72f4302413fe48ca23bb0a211848f90cd4025016
Melvin Peepers <peepersm@gmail.com>
2019-05-03 14:46:47 -0400

    Created a nav-link mouseover event for the header nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+// #1 mouseover event listener
+const navList = document.querySelectorAll('nav a');
+// console.log(navList);
+navList.forEach((link, i) => {
+    link.addEventListener("mouseover", function() {
+        link.style.color = '#B1D5E1';
+
+        setTimeout(function() {
+            link.style.color = "#000";
+        }, 500);
+    }, false);
+});
+
+
